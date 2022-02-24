98544eb7522bfb404db09700fcb2c97a28524195
Oskar Vatic <oskar.vatic@sbgemomics.com>
2022-02-24 19:21:28 +0100

    read input file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import utils.FileUtils;
 
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("test");
+    public static void main(String[] args) throws IOException {
+        FileUtils fileUtils = new FileUtils();
+        InputStream is = new FileUtils().getFileAsIOStream("a_an_example.in.txt");
+        fileUtils.printFileContent(is);
     }
 }
---
+++ src/main/java/utils/FileUtils.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class FileUtils {
+  public InputStream getFileAsIOStream(final String fileName) {
+    InputStream ioStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
+
+    if (ioStream == null) {
+      throw new IllegalArgumentException(fileName + " not found.");
+    }
+    return ioStream;
+  }
+
+  public void printFileContent(InputStream is) throws IOException {
+    try (InputStreamReader isr = new InputStreamReader(is);
+         BufferedReader br = new BufferedReader(isr)) {
+      String line;
+      while ((line = br.readLine()) != null) {
+        System.out.println(line);
+      }
+      is.close();
+    }
+  }
+}
